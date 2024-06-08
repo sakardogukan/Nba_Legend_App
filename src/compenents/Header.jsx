40ee4f6299a11603cb8befeac5e817fa37f7a930
DGKN <sakardogukan@gmail.com>
2024-06-08 21:25:05 +0300

    Header.jsx file updated

--- src/compenents/Header.jsx
+++ src/compenents/Header.jsx
@@ -1,5 +1,5 @@
-import Container from "react-bootstrap/Container" //! bu yöntem performanslı olan yöntem
-import { Image } from "react-bootstrap" //! bu yöntem diğer yönteme göre daha az tavsiye edilen
+import Container from "react-bootstrap/Container" //! bu yöntem performanslı
+import { Image } from "react-bootstrap" //! bu yöntem diğer yönteme göre daha az kullanılıyor
 import logo from "../assets/nba-logo.png"
 
 const Header = () => {
